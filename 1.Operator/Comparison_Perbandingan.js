e6eb107988f03f2f403f81a18029407f1914044d
jhon <117662171+masjhon@users.noreply.github.com>
2023-10-18 22:51:32 +0700

    Update and rename Comparison.js to Comparison_Perbandingan.js

---
+++ 1.Operator/Comparison_Perbandingan.js
@@ -0,0 +1,8 @@
+  == // Sama Dengan
+  != // Tidak Sama Dengan
+  === // Strict sama dengan
+  !== // Strich tidak sama dengan
+  >  // Lebih besar dari
+  <  // Lebih kecil dari
+  <=  // Kurang kecil sama dengan
+  >=  // Kurang besar sama dengam
